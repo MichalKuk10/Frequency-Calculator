93c385ab7fa5ddce55f30877f2fa50521bd0f846
michal <kuk.michal3@gmail.com>
2021-05-26 14:40:49 +0200

    add words validators to check it's size with and without special chars

--- src/main/java/Calculator/Service/CalculatorService.java
+++ src/main/java/Calculator/Service/CalculatorService.java
@@ -9,4 +9,25 @@ public class CalculatorService {
     private static final List<Character> specialChars = Arrays.asList(' ','!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',',
             '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~');
 
+
+
+    public int checkNumberOfLettersInGivenWord(String word){
+        int numberOfOurWordLettersInInput = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (logicWord.contains(word.charAt(i))) {
+                numberOfOurWordLettersInInput++;
+            }
+        }
+        return numberOfOurWordLettersInInput;
+    }
+
+    public  int checkNumberOfLettersWithoutSpecialChar(String word){
+        int numberOfLettersInInput = 0;
+        for (int i = 0; i<word.length(); i++){
+            if (!specialChars.contains(word.charAt(i))){
+                numberOfLettersInInput++;
+            }
+        }
+        return numberOfLettersInInput;
+    }
 }
