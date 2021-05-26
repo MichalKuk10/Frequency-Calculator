984630800dc7a179b7c77965e853f37b8def5660
michal <kuk.michal3@gmail.com>
2021-05-26 14:29:15 +0200

    add model structure

--- src/main/java/Calculator/Model/SingleWordData.java
+++ src/main/java/Calculator/Model/SingleWordData.java
@@ -1,4 +1,52 @@
 package Calculator.Model;
 
+import java.util.Set;
+
 public class SingleWordData {
+
+    private final Set<Character> matchingChars;
+    private final int lengthOfWord;
+    private final int matchingOccurrencesNumber;
+
+    public SingleWordData(Builder builder){
+        this.matchingChars = builder.matchingChars;
+        this.lengthOfWord = builder.lengthOfWord;
+        this.matchingOccurrencesNumber = builder.matchingOccurrencesNumber;
+    }
+
+    public Set<Character> getMatchingChars() {
+
+        return matchingChars;
+    }
+
+    public int getLengthOfWord() {
+        return lengthOfWord;
+    }
+
+    public int getMatchingOccurrencesNumber() {
+        return matchingOccurrencesNumber;
+    }
+
+    public static class Builder{
+        private Set<Character> matchingChars;
+        private int lengthOfWord;
+        private int matchingOccurrencesNumber;
+
+        public Builder matchingChars(final Set<Character> matchingChars){
+            this.matchingChars = matchingChars;
+            return this;
+        }
+        public Builder lengthOfWords(final int lengthOfWord){
+            this.lengthOfWord = lengthOfWord;
+            return this;
+        }
+        public Builder matchingOccurrencesNumber(final int matchingOccurrencesNumber){
+            this.matchingOccurrencesNumber = matchingOccurrencesNumber;
+            return this;
+        }
+
+        public SingleWordData build(){
+            return new SingleWordData(this);
+        }
+    }
 }
