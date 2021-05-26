706d49ab4cdc98e055e67b5bb67a06f977f893bb
michal <kuk.michal3@gmail.com>
2021-05-26 20:57:50 +0200

    add readInput comments

--- src/main/java/Calculator/Database/FileReader.java
+++ src/main/java/Calculator/Database/FileReader.java
@@ -3,6 +3,7 @@ package Calculator.Database;
 public class FileReader  implements  InputReader{
     @Override
     public String readInput() {
+        //TODO: application can be extended and take inputs from different resources
         return null;
     }
 }
