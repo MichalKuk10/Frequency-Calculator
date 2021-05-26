c033c7d96a9d51f08e44549e7b51b6be42a21075
michal <kuk.michal3@gmail.com>
2021-05-26 14:59:25 +0200

    add first test to validate if method creates appropriate object values

---
+++ src/test/java/CalculatorServiceTests.java
@@ -0,0 +1,30 @@
+package java;
+
+import Calculator.Model.SingleWordData;
+import Calculator.Service.CalculatorService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CalculatorServiceTests {
+
+    @Test
+    public void should_check_if_creates_object_with_correct_values(){
+        //given
+        CalculatorService service = new CalculatorService();
+        String word = "House";
+        Set<Character> tempCharContained = new HashSet<>();
+        tempCharContained.add('O');
+        SingleWordData singleWordInput = new SingleWordData.Builder().matchingChars(tempCharContained).lengthOfWords(5).matchingOccurrencesNumber(1).build();
+
+        //when
+        List<SingleWordData> accualResult = service.createSingleWordInput(word);
+
+        //then
+        Assertions.assertEquals(accualResult.get(0).getLengthOfWord(), singleWordInput.getLengthOfWord());
+    }
+}
