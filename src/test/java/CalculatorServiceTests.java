a112ff985608fcc52d49f4fe37b95e36228a5bf3
michal <kuk.michal3@gmail.com>
2021-05-26 20:32:16 +0200

    add test to check if correctly counts "LOGIC" char occurences

--- src/test/java/CalculatorServiceTests.java
+++ src/test/java/CalculatorServiceTests.java
@@ -10,20 +10,29 @@ import java.util.List;
 import java.util.Set;
 
 public class CalculatorServiceTests {
+    CalculatorService service = new CalculatorService();
+    String word = "House!!!";
 
     @Test
     public void should_check_if_creates_object_with_correct_values(){
         //given
-        CalculatorService service = new CalculatorService();
-        String word = "House!!!";
         Set<Character> tempCharContained = new HashSet<>();
         tempCharContained.add('O');
         SingleWordData singleWordInput = new SingleWordData.Builder().matchingChars(tempCharContained).lengthOfWords(8).matchingOccurrencesNumber(1).build();
 
         //when
-        List<SingleWordData> accualResult = service.createSingleWordInput(word);
+        List<SingleWordData> accualResult = service.createSingleWordData(word);
 
         //then
         Assertions.assertEquals(accualResult.get(0).getLengthOfWord(), singleWordInput.getLengthOfWord());
+    }
+
+    @Test
+    public void should_check_if_correctly_counts_number_of_given_letters_in_word(){
+        //given
+        //when
+        int expectedNumber = service.checkNumberOfLettersInGivenWord(word.toUpperCase());
+        //then
+        Assertions.assertEquals(1, expectedNumber);
     }
 }
