b4bb9941fbc4c851a1de073c16bd98d19f426c8f
michal <kuk.michal3@gmail.com>
2021-05-26 14:49:12 +0200

    add controller logic

--- src/main/java/Calculator/Controller/CalculatorController.java
+++ src/main/java/Calculator/Controller/CalculatorController.java
@@ -1,12 +1,39 @@
 package Calculator.Controller;
 
+import Calculator.Database.InputReader;
+import Calculator.Database.UserInput;
+import Calculator.Model.SingleWordData;
 import Calculator.Service.CalculatorService;
+import Calculator.View.CalculatorView;
+
+import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.List;
 
 public class CalculatorController {
+    private final CalculatorService calculatorService = new CalculatorService();
+    private final InputReader inputReader = new UserInput();
+    private final CalculatorView calculatorView = new CalculatorView();
 
-    CalculatorService calculatorService = new CalculatorService();
 
-    public void countOccurrences(){
+    public void countOccurrences(String input){
+        String inputToUpperCase = input.toUpperCase();
+        int numberOfLettersInGivenWord = calculatorService.checkNumberOfLettersInGivenWord(inputToUpperCase);
+        int occurrencesWithoutSpecialChar = calculatorService.checkNumberOfLettersWithoutSpecialChar(inputToUpperCase);
+        List<SingleWordData> singleWordInput = calculatorService.createSingleWordInput(inputToUpperCase);
+
+        Collections.sort(singleWordInput);
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        singleWordInput.forEach(singleWord -> {
+            double v = (double) singleWord.getMatchingOccurrencesNumber() / (double) numberOfLettersInGivenWord;
+            String formatToDecimal = decimalFormat.format(v);
+            calculatorView.print(singleWord, formatToDecimal, numberOfLettersInGivenWord);
+        });
+
+        String totalFrequency = decimalFormat.format((double)numberOfLettersInGivenWord/occurrencesWithoutSpecialChar);
+        calculatorView.printTotalFrequency(totalFrequency, occurrencesWithoutSpecialChar, numberOfLettersInGivenWord);
 
     }
 }
