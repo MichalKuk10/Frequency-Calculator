84b50bd6a752a4a5a0a92a1acf5c51913c1e1511
michal <kuk.michal3@gmail.com>
2021-05-26 14:49:53 +0200

    add view

--- src/main/java/Calculator/View/CalculatorView.java
+++ src/main/java/Calculator/View/CalculatorView.java
@@ -1,6 +1,16 @@
 package Calculator.View;
 
+import Calculator.Model.SingleWordData;
+
 public class CalculatorView {
 
+    public void print(SingleWordData singleWord, String formatToDecimal, int numberOfLettersInGivenWord){
+        System.out.println("{" + singleWord.getMatchingChars()+ ", " + singleWord.getLengthOfWord() + "} = " + formatToDecimal + " (" + singleWord.getMatchingOccurrencesNumber() + "/" + numberOfLettersInGivenWord +")");
 
+    }
+
+    public void printTotalFrequency(String totalFrequency, int occurencesWithoutSpecialChar, int numberOfLettersInGivenWord){
+        System.out.println("TOTAL Frequency: " + totalFrequency + " (" + numberOfLettersInGivenWord + "/" + occurencesWithoutSpecialChar + ")");
+
+    }
 }
